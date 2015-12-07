043970ca6ec50a6781433a7ef60531192a10cdce
unknown <razd@obsidian.corp.jonespub.com>
2015-12-07 14:03:48 -0600

    Initial Project Creation

---
+++ ProjectsInJavaScriptAndJQuery/Project 2/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	// Set Options
+	var speed = 500; // fade speed
+	var autoswitch = true; // auto slider option
+	var autoswitch_speed = 4000; // auto slider speed
+
+	// alert('what?');
+	// add initial active class
+	$('.slide').first().addClass('active');
+
+	// hide all slides
+	$('.slide').hide();
+
+	//show first slide
+	$('.active').show();
+	
+	// Next Handler
+	$('#next').on('click', nextSlide);
+
+	// Prev handler
+	$('#prev').on('click', prevSlide);
+
+	// Auto Slider
+	if(autoswitch == true) {
+		setInterval(nextSlide, autoswitch_speed);
+	}
+
+	// Switch to next slide
+	function nextSlide() {
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':last-child')){
+			$('.slide').first().addClass('active');
+		} else {
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+
+	// Switch to prev slide
+	function prevSlide() {
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':first-child')){
+			$('.slide').last().addClass('active');
+		} else {
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+
+	}
+
+});
